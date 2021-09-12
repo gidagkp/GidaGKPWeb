6396911cf67ed0d5c215f50dacc22ea23c0ca7df
Amendra Dutt <amendra.dutt@gmail.com>
2021-09-12 23:19:13 +0530

    Add Mortagedetails

--- GidaGkpWeb/BAL/Masters/MortageDetails.cs
+++ GidaGkpWeb/BAL/Masters/MortageDetails.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DataLayer;
+using GidaGkpWeb.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,43 @@ namespace GidaGkpWeb.BAL.Masters
 {
     public class MortageDetails
     {
+        GidaGKPEntities _db = null;
+        public MortgageModel GetmortgageDetailById(int ApplicationId)
+        {
+            try
+            {
+                _db = new GidaGKPEntities();
+                return (from AppApplication in _db.ApplicantDetails
+                        join ApplicantDet in _db.ApplicantPlotDetails on AppApplication.ApplicationId equals ApplicantDet.ApplicationId
+                        join SectorNameLookup in _db.Lookups on ApplicantDet.SectorName equals SectorNameLookup.LookupId
+                        join plotMaster in _db.PlotMasters on SectorNameLookup.LookupId equals plotMaster.SectorName
+                        join allotment in _db.AllocateAllotmentDetails on AppApplication.ApplicationId equals allotment.ApplicationId
+                        where AppApplication.ApplicationId == ApplicationId
+                        select new MortgageModel
+                        {
+                            //Id = ApplicantDet.Id,
+                            //FullName = ApplicantDet.FullApplicantName,
+                            AddressOfApplicant = AppApplication.PAddress,
+                            SectorName = ApplicantDet.SectorName.ToString(),
+                            PlotNo = plotMaster.PlotNumber,
+                            AllotmentId = allotment.Id,
+                            DateOfMortgageRequestLetter = allotment.DateofAllotmentLetter
+
+
+
+                        }).FirstOrDefault();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(e));
+                    }
+                }
+                return null;
+            }
+        }
     }
 }
